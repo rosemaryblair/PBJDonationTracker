9328790d9ac558af18e77b274611400862a4d682
kdai7 <35241631+kdai7@users.noreply.github.com>
2018-09-16 16:20:50 -0400

    Added button activity to cancel button on LoginActivity to return to MainActivity

--- app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/LoginActivity.java
+++ app/src/main/java/edu/gatech/micheyang/pbjdonationtracker/LoginActivity.java
@@ -1,10 +1,12 @@
 package edu.gatech.micheyang.pbjdonationtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
@@ -24,6 +26,12 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void onCancelPressed(View view) {
+        Log.d("Edit", "canceled");
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
